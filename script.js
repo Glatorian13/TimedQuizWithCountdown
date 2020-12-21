592d234618e9a4457a7f9f8f3b7da820a3cc20f1
Javier Cardenas <glatorian@gmail.com>
2020-12-20 23:58:12 -0500

    working on javascript side, still not working

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+var questions = [{
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "Which of these is not used to loop?",
+    choices: ["for", "while", "foreach", "sequence"],
+    answer: "sequence"
+  },
+  {
+    question: "Which of these is not a way to save a variable",
+    choices: ["vet", "var", "let", "const"],
+    answer: "vet"
+  },
+  {
+    question: "JS date function starts in seconds to current day from what day in 1970",
+    choices: ["January 1", "December 31", "June 1", "April 23"],
+    answer: "January 1"
+}];
 
-// Initialize //
+//timer for quiz
+var timer = document.getElementById("timer");
+var endScore = document.querySelector("#scoreBoard");
+var submitScoreBtn = document.getElementById("enterScorebtn");
+var inputName = document.getElementById("nameInput");
 
+var timeLeftSec = 60;
+function setTime() {
+    var timeInterval = setInterval(function() {
+        timeLeftSec--;
+        console.log(timeLeftSec);
+        timer.textContent = "Time: " + timeLeftSec;
+
+        if(timeLeftSec === 0) {
+            clearInterval(timeInterval);
+            questionSlide.setAttribute("style", "display:none");
+            landingCard.setAttribute("style", "display: block");
+            endScore.textContent = "Your score is: " + timeLeftSec;
+
+        }
+    }, 1000);
 }
+
+//start of quiz
+
+var startBtn = document.getElementById("#")
+var questionCard = 
+var landingCardVar = 
